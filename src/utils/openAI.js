c552e7e364b05d2592c69514c9254296be1bf947
Vinay Deshmukh <66013055+Vinay-1312@users.noreply.github.com>
2023-10-26 13:26:54 +0100

    Update openAI.js

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -1,9 +1,5 @@
 import OpenAI from 'openai';
 import { abcd } from './constant';
-<<<<<<< HEAD
-
-=======
->>>>>>> 2b86f42359e16799f846c899344bf4ef27348e25
 const openai = new OpenAI({
   apiKey: abcd,
   dangerouslyAllowBrowser:true // defaults to process.env["OPENAI_API_KEY"]
